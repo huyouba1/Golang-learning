98a01b07d32b7219ff46d3e9fbd6d9694a49c9bf
zhenxiyao <huyouba1@163.com>
2022-10-12 16:14:33 +0800

    Day02-04 todolist

--- Day03/course/todolist/todolist_v2.go
+++ Day03/course/todolist/todolist_v2.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // 做一个命令行的任务管理器
@@ -66,6 +67,7 @@ func printTask(task map[string]string) {
 	fmt.Println("任务名:", task[name])
 	fmt.Println("开始时间:", task[startTime])
 	fmt.Println("完成时间:", task[endTime])
+	fmt.Println("任务状态:", task[status])
 	fmt.Println(strings.Repeat("-", 20))
 
 }
@@ -106,7 +108,7 @@ func add() {
 
 func query() {
 	// all 显示全部
-	q := input("请输入查询信息：")
+	q := input("请输入查询信息名称：")
 	//var text string
 	//fmt.Print("请输入查询信息：")
 	//fmt.Scan(&text)
@@ -118,20 +120,106 @@ func query() {
 }
 
 // modify
+func modify() {
+
+	for _, tname := range todos {
+		printTask(tname)
+	}
+	uid := input("请输入要修改的任务ID：")
+	var todo map[string]string
+	for _, task := range todos {
+		if task[id] == uid {
+			todo = task
+		}
+	}
+	//fmt.Println(uid)
+	if todo != nil {
+		fmt.Println("要修改的用户信息：")
+		printTask(todo)
+		inputInfo := input("是否确定修改？(y/Y/YES/yes)")
+		if inputInfo == "y" || inputInfo == "Y" || inputInfo == "yes" || inputInfo == "YES" {
+			todo[name] = input("新任务名:")
+			todo[startTime] = input("开始时间:")
+			todo[status] = input("状态:")
+			if todo[status] == "完成" {
+				todo[endTime] = time.Now().Format("2006-01-02 15:15:04:05")
+			}
+			fmt.Println("修改成功!")
+		} else {
+			fmt.Println("输入指令有误 (y/Y/YES/yes)")
+		}
+	} else {
+		fmt.Println("输入的任务ID不存在")
+	}
+
+}
+
 // delete
+func remove() {
+	for _, tname := range todos {
+		printTask(tname)
+	}
+	uid := input("请输入要删除的任务ID：")
+	var todo map[string]string
+	for index, task := range todos {
+		if task[id] == uid {
+			todo = task
+			if todo != nil {
+				inputInfo := input("是否确定删除？(y/Y/YES/yes)")
+				if inputInfo == "y" || inputInfo == "Y" || inputInfo == "yes" || inputInfo == "YES" {
+					copy(todos[index:], todos[index+1:])
+					todos = todos[:len(todos)-1]
+					fmt.Println("删除成功")
+					fmt.Println(todos)
+
+				} else {
+					fmt.Println("删除操作取消")
+					return
+				}
+			} else {
+				fmt.Println("输入的任务ID不存在")
+				return
+			}
+		}
+	}
+
+}
+
 // detail
+func check() {
+	var result string
+	taskName := input("请输入任务名称，检测是否存在:")
+	for _, todo := range todos {
+		if taskName == todo[name] {
+			//fmt.Printf("%#v\n", todo[name])
+			//fmt.Printf("%#v\n", taskName)
+			//fmt.Println("任务已存在")
+			result = "任务已存在"
+			break
+		} else {
+			//fmt.Printf("%#v\n", todo[name])
+			//fmt.Printf("%#v\n", taskName)
+			//fmt.Println("任务bu存在")
+			result = "任务bu存在"
+		}
+	}
+	fmt.Println(result)
+}
 
 func main() {
 	methods := map[string]func(){
-		"add":   add,
-		"query": query,
+		"add":    add,
+		"query":  query,
+		"modify": modify,
+		"remove": remove,
+		"check":  check,
 	}
 	for {
 		//text := input("请输入操作(add/query/exit/.../)：")
 		//fmt.Println("请输入操作(add/query/exit/.../)：")
 		//var text string
 		//fmt.Scan(&text)
-		text := input("请输入操作(add/query/exit/.../)：")
+		text := input("请输入操作(add/query/exit/modify/remove/check/.../)：")
 		if text == "exit" {
 			break
 		}
