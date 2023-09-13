97432a43cdaef20c76ebccd803f04b3fb6da860c
zhenxiyao <huyouba1@163.com>
2023-09-13 21:21:52 +0800

    补充restful框架

--- kk/Day09/course/2-addr.go
+++ kk/Day09/course/2-addr.go
@@ -9,6 +9,17 @@ func main() {
 	// 获取网络地址
 	addrs, _ := net.InterfaceAddrs()
 	for _, addr := range addrs {
-		fmt.Println(addr.Network(), addr.String())
+		//fmt.Println(addr.Network(), addr.String())
+		switch v := addr.(type) {
+		case *net.IPNet:
+			if !v.IP.IsLoopback() && v.IP.To4() != nil {
+				fmt.Println("本地IPv4地址：", v.IP.String())
+			}
+		case *net.IPAddr:
+			if !v.IP.IsLoopback() && v.IP.To4() != nil {
+				fmt.Println("本地IPv4地址：", v.IP.String())
+			}
+			//fmt.Println(addr.String())
+		}
 	}
 }
